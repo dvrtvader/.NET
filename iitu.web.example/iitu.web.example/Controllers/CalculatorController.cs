ed2f6c3b2e4eade8f2f1094ecd11dba572205ce6
dvrtvader <44144700+dvrtvader@users.noreply.github.com>
2019-10-12 01:44:42 +0600

    updated some mistakes

--- iitu.web.example/iitu.web.example/Controllers/CalculatorController.cs
+++ iitu.web.example/iitu.web.example/Controllers/CalculatorController.cs
@@ -8,9 +8,30 @@ namespace iitu.web.example.Controllers
 {
     public class CalculatorController : Controller
     {
-        public IActionResult Index()
+        [Route("{firstNumber:int}/{secondNumber:int}")]
+        public IActionResult Sum(int firstNumber, int secondNumber)
         {
-            return View();
+            ViewData["action"] = RouteData.Values["action"].ToString();
+            ViewData["mark"] = '+';
+            ViewData["firstNumber"] = firstNumber;
+            ViewData["secondNumber"] = secondNumber;
+            ViewData["result"] = firstNumber + secondNumber;
+
+            return View("Result");
+        }
+
+
+
+        [Route("{firstNumber:int}/{secondNumber:int:min(1)}")]
+        public IActionResult Divide(int firstNumber, int secondNumber)
+        {
+            ViewData["action"] = RouteData.Values["action"].ToString();
+            ViewData["mark"] = '/';
+            ViewData["firstNumber"] = firstNumber;
+            ViewData["secondNumber"] = secondNumber;
+            ViewData["result"] = firstNumber / secondNumber;
+
+            return View("Result");
         }
     }
 }
