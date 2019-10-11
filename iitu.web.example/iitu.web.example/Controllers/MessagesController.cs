17df1dfae2683aad4d51c6f865d5a7aeb4a0f58d
dvrtvader <44144700+dvrtvader@users.noreply.github.com>
2019-10-12 01:14:28 +0600

    Second tutorial for .net
    Continued after first tutorial

---
+++ iitu.web.example/iitu.web.example/Controllers/MessagesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iitu.web.example.Controllers
+{
+    public class MessagesController : Controller
+    {
+        public IActionResult ShowMessage(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                ViewData["Message"] = "Message is empty";
+            }
+            else
+            {
+                ViewData["Message"] = message;
+            }
+
+            return View();
+        }
+    }
+}
